7d8417550c5c131c38e723e29eefdc340d65bd24
Bochun Bai <bbc@sinofool.net>
2020-06-25 23:40:16 -0700

    Fix short-tag

--- src/iosplist.php
+++ src/iosplist.php
@@ -6,7 +6,8 @@ $PSI = new \ca\gearzero\psiapp\Psi();
 
 $plist = $PSI->parse_ipa($app_name, $ipa_name);
 header("Content-Type: application/xml");
-?><?xml version="1.0" encoding="UTF-8"?>
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
     <dict>
